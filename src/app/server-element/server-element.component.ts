7685323f7a24eefcfd3ef7cd94a9f81aaa80413a
Alexey Semidotsky <asemidotsky@gmail.com>
2018-04-03 20:28:52 +0300

    ContentChild

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,13 +1,24 @@
-import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  OnChanges,
+  AfterContentInit,
+  SimpleChanges,
+  ContentChild,
+  ElementRef
+} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css']
 })
-export class ServerElementComponent implements OnInit, OnChanges {
+export class ServerElementComponent implements OnInit, OnChanges, AfterContentInit {
   // tslint:disable-next-line:no-input-rename
   @Input('srvElement') element: { type: string, name: string, content: string };
+
+  @ContentChild('contentParagraph') contentParagraph: ElementRef;
 
   constructor() {
     console.log('constructor called!');
@@ -15,10 +26,16 @@ export class ServerElementComponent implements OnInit, OnChanges {
 
   ngOnInit() {
     console.log('ngInit called!');
+    console.log('Text Content of paragraph: ' + this.contentParagraph.nativeElement.textContent);
   }
 
   ngOnChanges(changes: SimpleChanges): void {
     console.log(`ngOnChanges called!`);
     console.log(changes);
+  }
+
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit called!');
+    console.log('Text Content of paragraph: ' + this.contentParagraph.nativeElement.textContent);
   }
 }
