e6238716d6a8d104087b8f11e5e5a418cb4e6199
Alexey Semidotsky <asemidotsky@gmail.com>
2018-04-03 12:36:13 +0300

    basic layout for components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'Test description',
+      'http://picturetherecipe.com/wp-content/uploads/2013/03/PTR-Balsamic-Glazed-Steak-Rolls1.jpg'),
+    new Recipe('Second recipe', 'Some other description',
+      'http://picturetherecipe.com/wp-content/uploads/2013/03/PTR-Balsamic-Glazed-Steak-Rolls1.jpg')
+  ];
 
   constructor() { }
 
