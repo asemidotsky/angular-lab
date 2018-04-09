842e812aee6fd78545bd8fa3e2adfe38a7e7d305
Alexey Semidotsky <asemidotsky@gmail.com>
2018-04-09 12:05:30 +0300

    interaction of services

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,25 @@
+import { Ingredient } from '../shared/ingredient.model';
+import { EventEmitter } from '@angular/core';
+
 export class ShoppingListService {
-    
+    ingredientsChanged = new EventEmitter<Ingredient[]>();
+
+    private _ingredients: Ingredient[] = [
+        new Ingredient('Apples', 5),
+        new Ingredient('Tomatos', 10),
+      ];
+
+    public get ingredients(): Ingredient[] {
+        return this._ingredients.slice();
+    }
+
+    addIngredient(ingredient: Ingredient) {
+        this._ingredients.push(ingredient);
+        this.ingredientsChanged.emit(this._ingredients.slice())
+    }
+
+    addIngredients(ingredients: Ingredient[]) {
+        this._ingredients.push(...ingredients);
+        this.ingredientsChanged.emit(this._ingredients.slice());
+    }
 }
